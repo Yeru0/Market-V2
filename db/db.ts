0810826808d5f1dfe8079629338a809ac7bd2141
Horváth Péter <yerulearn065077@gmail.com>
2025-04-09 14:06:32 +0200

    Rando com jon a bus
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- db/db.ts
+++ db/db.ts
@@ -6,73 +6,76 @@ import { resolve } from "node:path";
 
 export const readTable: (tableName: string) => Promise<Table> | any = async (tableName: string) => {
 
-    
+
     // Slices off the header from data, puts the header into a separate
     // array, takes out the string from that array, then splits the
     // string apart into separate array elements into a new array
     let table: Record[] = [];
-    
+
     return new Promise((resolve, reject) => {
-        
-        let data: string[] = []
+
+        let data: string[] = [];
 
         readLbL(`${tableName}.csv`).then((result) => {
-            data = result
-            
+            data = result;
+
             let header: string[] = data.splice(0, 1)[0].split(",");
 
-        
-        if (data.length === 0 || data.length === 1) reject("DB ERROR: Given table empty")
-        
 
-        try {
-            for (const lines of data) {
-                let line = lines.split(",");
-                table.push(new Record(line, header));
+            if (data.length === 0 || data.length === 1) reject("DB ERROR: Given table empty");
+
+
+            try {
+                for (const lines of data) {
+                    let line = lines.split(",");
+                    table.push(new Record(line, header));
+                }
+
+                resolve(new Table(header, table, tableName));
+
+            } catch (err) {
+                reject(err);
             }
 
-            resolve(new Table(header, table, tableName));
+        }).catch((err) => {
+            console.log(err);
 
-        } catch (err) {
+            if (err.code === "ENOENT") reject(`DB ERROR: Table with name "${tableName}" doesn't exist or is not accessible.`);
             reject(err);
-        }
-
-    }).catch((err) => {
-        if (err == `DB ERROR: The file ${tableName}.csv does not exist or is not accessible.`) reject(`DB ERROR: Table with name ${tableName} doesn't exist or is not accessible.`)
-        reject(err)
-    })
+        });
 
     });
 
 };
 
-export const writeTable: (tableName: string, tableValue: Table, overwrite?:boolean) => Promise<string> = async (tableName: string, tableValue: Table, overwrite?:boolean) => {
+export const writeTable: (tableName: string, tableValue: Table, overwrite?: boolean) => Promise<string> = async (tableName: string, tableValue: Table, overwrite?: boolean) => {
     return new Promise((resolve, reject) => {
 
-        let data: string[] = []
-        
-        data.push(tableValue.getHeader().toString())
+        let data: string[] = [];
+
+        data.push(tableValue.getHeader().toString());
 
         for (const record of tableValue.getRecords()) {
-            let properties: string[] = []
+            let properties: string[] = [];
             for (const [key, property] of Object.entries(record)) {
-                properties.push(property)
+                properties.push(property);
             }
-            data.push(properties.toString())
+            data.push(properties.toString());
         }
 
         writeLbL(`${tableName}.csv`, data, overwrite).then((result) => {
-            resolve(result)
+            resolve(result);
         }).catch((err) => {
-            reject(err)
-        })
+            reject(err);
+        });
 
-    })
-}
+    });
+};
 
-readTable("as").catch((err) => {
+readTable("asd").catch((err) => {
     console.log(err);
-    
-})
+
+});
 
 /* TODO not handled empty table nonexistent */
+// TODO SET TYPE ON PROMISES AS WELL
--- db/fileFunc.ts
+++ db/fileFunc.ts
@@ -1,47 +1,42 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import readline from "node:readline";
 
 // Reads any file line by line and returns them into an object
 export function readLbL(fileName: string): Promise<string[]> {
-    return new Promise((resolve: (value: string[]) => void, reject: (value: Error|string) => void) => {
+    return new Promise((resolve: (value: string[]) => void, reject: (value: Error | string) => void) => {
 
-        try {
+        fs.createReadStream(`db/tables/${fileName}`).then((result) => {
 
-            fs.access(fileName, fs.constants.F_OK, (err) => {
-                if (err) {
-                  reject(`DB ERROR: The file "${fileName}" does not exist or is not accessible.`);
-                }
-              });
+            let fileStream = result;
 
-            const fileStream = fs.createReadStream(`db/tables/${fileName}`);            
 
-            const rl = readline.createInterface({
-                input: fileStream,
-                crlfDelay: Infinity, // To handle different line endings properly
-            });
+        }).catch((err) => {
+            // TODO ITT TARTOTTal
+        });
 
-            let lines: string[] = [];
-            // Pushes the line every time the a new line is read
-            rl.on("line", (line) => {
-                lines.push(line.trim());
-            });
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity, // To handle different line endings properly
+        });
 
-            // When the stream closes
-            rl.on("close", () => {
-                resolve(lines);
-            });
+        let lines: string[] = [];
+        // Pushes the line every time the a new line is read
+        rl.on("line", (line) => {
+            lines.push(line.trim());
+        });
 
-        } catch (err) {          
-            reject(err);
-        }
+        // When the stream closes
+        rl.on("close", () => {
+            resolve(lines);
+        });
 
     });
 };
 
-export function writeLbL(fileName: string, linesToWrite:string[], overwrite:boolean = false): Promise<string> {
+export function writeLbL(fileName: string, linesToWrite: string[], overwrite: boolean = false): Promise<string> {
     return new Promise((resolve, reject) => {
-        let header = linesToWrite.splice(0,1)
-        if(overwrite) {
+        let header = linesToWrite.splice(0, 1);
+        if (overwrite) {
             fs.writeFile(`db/tables/${fileName}`, `${header[0]}`, (err) => {
                 if (err) {
                     reject(err);
@@ -57,7 +52,7 @@ export function writeLbL(fileName: string, linesToWrite:string[], overwrite:bool
             });
         }
 
-        resolve("finished")
+        resolve("finished");
 
 
     });

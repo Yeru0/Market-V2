3a90b41ef6ba2d680cda3242bb0824a19bb103aa
Horváth Péter <yerulearn065077@gmail.com>
2025-04-24 14:03:54 +0200

    Add inactive products to the end of the storage list, also make render products own component also in storage
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/routes/storage/+page.svelte
+++ src/routes/storage/+page.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-    import AddOverlay from "./addOverlay.svelte";
-    import UpdateOverlay from "./updateOverlay.svelte";
-    import DeleteOverlay from "./deleteOverlay.svelte";
+    import AddOverlay from "./AddOverlay.svelte";
 	import { Product } from "$lib/siteObjects.svelte";
+	import RenderProds from "./RenderProds.svelte";
 
     let { data } = $props()
 
     //Render products
+    let parsedProds: Product[] = [] // This is needed so I can add the inactive products to the end
     let products: Product[] = $state([])
+    let inactiveProducts: Product[] = [] // This is needed so I can add the inactive products to the end
 
     for (const product of data.products) {
         // svelte-ignore state_referenced_locally
-        products.push(new Product(product))
+        parsedProds.push(new Product(product))
+    }
+
+    for (const product of parsedProds) {
+        // svelte-ignore state_referenced_locally
+        if (product.active) products.push(product)
+    }
+
+    for (const product of parsedProds) {
+        // svelte-ignore state_referenced_locally
+        if (!product.active) inactiveProducts.push(product)
     }
 
     // svelte-ignore state_referenced_locally
@@ -24,6 +35,25 @@
             return 0;
         }
     })
+
+    // svelte-ignore state_referenced_locally
+    inactiveProducts = [...inactiveProducts].sort((a: Product, b: Product) => {
+        if (a.name.toUpperCase() < b.name.toUpperCase()) {                
+            return -1;
+        } else if (a.name.toUpperCase() > b.name.toUpperCase()) {
+            return 1;
+        } else {           
+            return 0;
+        }
+    })
+    
+    // svelte-ignore state_referenced_locally
+    for (const product of inactiveProducts) {
+        products.push(product)
+    }
+
+
+
 </script>
 
 <svelte:head>
@@ -40,94 +70,9 @@
 
         <section>    
 
-            <h2>{product.name}</h2>
-            <div class="top-overlays">
-                <button onclick={() => { product.modOverlay = !product.modOverlay }}>Termék módosítása</button>
-                <button onclick={() => { product.delOverlay = !product.delOverlay }}>Termék törlése</button>
-            </div>
-
-            <div class="top-stats">
-                <div class="purchased stat-card">
-                    <h3>Beszerzett</h3>
-                    <p>{product.purchasedN} db</p>
-                </div>
-                <div class="purchased-price stat-card">
-                    <h3>Beszerzési ár</h3>
-                    <p>{product.purchasePriceM} Ft</p>
-                </div>
-                <div class="single-purchased-price stat-card">
-                    <h3>Beszerzési egységár</h3>
-                    <p>{product.singleProductValueM} Ft</p>
-                </div>
-                <div class="stored stat-card">
-                    <h3>Raktározva</h3>
-                    <p>{product.allRemainingN} db</p>
-                </div>
-                <div class="profit stat-card">
-                    <h3>Profit</h3>
-                    <p>{product.allProfitM} Ft</p>
-                </div>
-            </div>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Szervező</th>
-                        <th>Résztvevő</th>
-                        <th>Összes</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        <tr>
-                            <th>Eladott</th>
-                            <td>{product.soldToOrgN} db</td>
-                            <td>{product.soldToPartN} db</td>
-                            <td>{product.allSoldN} db</td>
-                        </tr>
-                        <tr>
-                            <th>Egységár</th>
-                            <td>{product.singleOrgPriceM} Ft</td>
-                            <td>{product.singlePartPriceM} Ft</td>
-                            <td>{product.singleProductValueM} Ft</td>
-                        </tr>
-                        <tr>
-                            <th>Haszonkulcs</th>
-                            <td>{product.organiserProfitMargin}%</td>
-                            <td>{product.participantProfitMargin}%</td>
-                            <td>- %</td>
-                        </tr>
-                        <tr>
-                            <th>Egységprofit</th>
-                            <td>{product.singleOrgProfitM} Ft</td>
-                            <td>{product.singlePartProfitM} Ft</td>
-                            <td>- Ft</td>
-                        </tr>
-                        <tr>
-                            <!-- Eladott áruk beszerzési értéke, eladott * egység beszerár -->
-                            <th>ELÁBÉ</th>
-                            <td>{product.valueOfSoldProductsOrgM} Ft</td>
-                            <td>{product.valueOfSoldProductsPartM} Ft</td>
-                            <td>{product.valueOfSoldProductsM} Ft</td>
-                        </tr>
-                        <tr>
-                            <th>Bevétel</th>
-                            <td>{product.allOrgIncomeM} Ft</td>
-                            <td>{product.allPartIncomeM} Ft</td>
-                            <td>{product.allIncomeM} Ft</td>
-                        </tr>
-                </tbody>
-            </table>    
-
+            <RenderProds bind:products {product}></RenderProds>
+        
         </section>
-
-        {#if product.modOverlay}
-            <UpdateOverlay {product} bind:products></UpdateOverlay>
-        {/if}
-
-        {#if product.delOverlay}
-            <DeleteOverlay {product} bind:products></DeleteOverlay>
-        {/if}
 
     {/each}
 
---
+++ src/routes/storage/RenderProds.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+
+    import UpdateOverlay from "./UpdateOverlay.svelte";
+    import DeleteOverlay from "./DeleteOverlay.svelte";
+
+    let {
+        product,
+        products = $bindable()
+    } = $props()
+    
+</script>
+
+
+<h2>{product.name}</h2>
+<div class="top-overlays">
+    <button onclick={() => { product.modOverlay = !product.modOverlay }}>Termék módosítása</button>
+    <button onclick={() => { product.delOverlay = !product.delOverlay }}>Termék törlése</button>
+</div>
+
+<div class="top-stats">
+    <div class="purchased stat-card">
+        <h3>Beszerzett</h3>
+        <p>{product.purchasedN} db</p>
+    </div>
+    <div class="purchased-price stat-card">
+        <h3>Beszerzési ár</h3>
+        <p>{product.purchasePriceM} Ft</p>
+    </div>
+    <div class="single-purchased-price stat-card">
+        <h3>Beszerzési egységár</h3>
+        <p>{product.singleProductValueM} Ft</p>
+    </div>
+    <div class="stored stat-card">
+        <h3>Raktározva</h3>
+        <p>{product.allRemainingN} db</p>
+    </div>
+    <div class="stored stat-card">
+        <h3>Kivett</h3>
+        <p>{product.takenOutN} db</p>
+    </div>
+    <div class="profit stat-card">
+        <h3>Profit</h3>
+        <p>{product.allProfitM} Ft</p>
+    </div>
+</div>
+
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>Szervező</th>
+            <th>Résztvevő</th>
+            <th>Összes</th>
+        </tr>
+    </thead>
+    <tbody>
+            <tr>
+                <th>Eladott</th>
+                <td>{product.soldToOrgN} db</td>
+                <td>{product.soldToPartN} db</td>
+                <td>{product.allSoldN} db</td>
+            </tr>
+            <tr>
+                <th>Egységár</th>
+                <td>{product.singleOrgPriceM} Ft</td>
+                <td>{product.singlePartPriceM} Ft</td>
+                <td>{product.singleProductValueM} Ft</td>
+            </tr>
+            <tr>
+                <th>Haszonkulcs</th>
+                <td>{product.organiserProfitMargin}%</td>
+                <td>{product.participantProfitMargin}%</td>
+                <td>- %</td>
+            </tr>
+            <tr>
+                <th>Egységprofit</th>
+                <td>{product.singleOrgProfitM} Ft</td>
+                <td>{product.singlePartProfitM} Ft</td>
+                <td>- Ft</td>
+            </tr>
+            <tr>
+                <!-- Eladott áruk beszerzési értéke, eladott * egység beszerár -->
+                <th>ELÁBÉ</th>
+                <td>{product.valueOfSoldProductsOrgM} Ft</td>
+                <td>{product.valueOfSoldProductsPartM} Ft</td>
+                <td>{product.valueOfSoldProductsM} Ft</td>
+            </tr>
+            <tr>
+                <th>Bevétel</th>
+                <td>{product.allOrgIncomeM} Ft</td>
+                <td>{product.allPartIncomeM} Ft</td>
+                <td>{product.allIncomeM} Ft</td>
+            </tr>
+    </tbody>
+</table>    
+
+
+{#if product.modOverlay}
+    <UpdateOverlay {product} bind:products></UpdateOverlay>
+{/if}
+
+{#if product.delOverlay}
+    <DeleteOverlay {product} bind:products></DeleteOverlay>
+{/if}

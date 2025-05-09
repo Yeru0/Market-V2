140c34842efb86b9ebe6d8b7152c367fdaa7a877
Yeru0 <yeru065077@gmail.com>
2025-05-09 22:32:34 +0200

    Exported websocket address to environment variable
    Signed-off-by: Yeru0 <yeru065077@gmail.com>

--- src/lib/shared.svelte.ts
+++ src/lib/shared.svelte.ts
@@ -1,8 +1,9 @@
 import { writable } from "svelte/store";
+import { PUBLIC_WEBSOCKET_ADDRESS } from "$env/static/public";
 
 export let priceListStateSellingToOrg = writable(false);
 
 export let priceListWebSocket = writable({
-    ws: new WebSocket("ws://192.168.50.81:8083/prices"),
+    ws: new WebSocket(`ws://${PUBLIC_WEBSOCKET_ADDRESS}:8083/prices`),
     id: ""
 });

e72633c5585198476728481db7a60bb6d8324f26
Horváth Péter <yerulearn065077@gmail.com>
2025-05-11 13:31:17 +0200

    Half done toasts
    Signed-off-by: Horváth Péter <yerulearn065077@gmail.com>

--- src/routes/Toast.svelte
+++ src/routes/Toast.svelte
@@ -29,17 +29,62 @@
     })
 
     onDestroy(() => {
+        show = false
+        progress = 0
+        elapsed = 0
+        time = 0
         clearInterval(interval)
     })
 
 </script>
 
-<section>
+<style>
 
-    <h2>{text}</h2>
+    .toast {
+        position: fixed;
+        bottom: var(--n-m);
+        left: 50%;
+        transform: translate(-50%, 0);
+        background-color: var(--c-g-scale-t1a);
 
+        display: grid;
+        place-items: center;
+        grid-template-columns: auto fit-content;
+        grid-template-rows: repeat(2, auto);
+        grid-template-areas:
+        "text button"
+        "progress progress";
+
+        & .text {
+            grid-area: text;
+            color: var(--c-default-t1);
+            font-weight: bolds;
+        }
+
+        & button {
+            grid-area: button;
+            width: min-content;
+            height: min-content;
+        }
+
+        & progress {
+            grid-area: progress;
+        }
+    }
+
+
+</style>
+
+<section class="toast">
+
+    <h4 class="text">{text}</h4>
+
+    <button onclick={() => { show = false }}>
+        <span class="material-symbols-outlined">
+            close
+        </span>
+    </button>
+    
     <progress id="file" value="{progress}" max="100">{progress.toFixed(0)}</progress>
-
-    <button onclick={() => { show = false }}>X</button>
 
 </section>
